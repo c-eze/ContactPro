9331f18cb4c6c03ba1c5f90c72b2b7d3f4797bb6
c-eze <c-ezeadmin@outlook.com>
2024-07-28 07:28:18 -0500

    Bind categories

--- ContactPro/Controllers/ContactsController.cs
+++ ContactPro/Controllers/ContactsController.cs
@@ -37,8 +37,25 @@ namespace ContactPro.Controllers
         [Authorize]
         public async Task<IActionResult> Index()
         {
-            var applicationDbContext = _context.Contacts.Include(c => c.AppUser);
-            return View(await applicationDbContext.ToListAsync());
+            var contacts = new List<Contact>();
+            string appUserId = _userManager.GetUserId(User);
+
+            //return the userID and its associated contacts and categories
+            AppUser? appUser = _context.Users
+                                      .Include(c => c.Contacts)
+                                      .ThenInclude(c => c.Categories)
+                                      .FirstOrDefault(u => u.Id == appUserId);
+
+            var categories = appUser.Categories;
+
+            contacts = appUser.Contacts.OrderBy(c => c.LastName)
+                                       .ThenBy(c => c.FirstName)
+                                       .ToList();
+
+            ViewData["CategoryId"] = new SelectList(categories, "Id", "Name");
+
+
+            return View(contacts);
         }
 
         // GET: Contacts/Details/5
--- ContactPro/Models/AppUser.cs
+++ ContactPro/Models/AppUser.cs
@@ -18,5 +18,9 @@ namespace ContactPro.Models
 
         [NotMapped]
         public string? FullName { get { return $"{FirstName} {LastName}"; } }
+
+        //Virtuals
+        public virtual ICollection<Contact> Contacts { get; set; } = new HashSet<Contact>();
+        public virtual ICollection<Category> Categories { get; set; } = new HashSet<Category>();
     }
 }
