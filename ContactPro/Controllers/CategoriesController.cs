52befeb257e0386bbb3fd96f2db51161b3af6992
c-eze <c-ezeadmin@outlook.com>
2024-07-29 22:43:50 -0500

    Category Edit - Create

--- ContactPro/Controllers/CategoriesController.cs
+++ ContactPro/Controllers/CategoriesController.cs
@@ -61,8 +61,7 @@ namespace ContactPro.Controllers
         // GET: Categories/Create
         [Authorize]
         public IActionResult Create()
-        {
-            ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id");
+        { 
             return View();
         }
 
@@ -73,13 +72,16 @@ namespace ContactPro.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,AppUserId,Name")] Category category)
         {
+            ModelState.Remove("AppUserId");
+
             if (ModelState.IsValid)
             {
+                string appUserId = _userManager.GetUserId(User);
+                category.AppUserId = appUserId;
                 _context.Add(category);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
-            }
-            ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", category.AppUserId);
+            } 
             return View(category);
         }
 
