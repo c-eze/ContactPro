c534477ffd87d3d31afcb7777f798560d5e2a4ce
c-eze <c-ezeadmin@outlook.com>
2024-07-30 07:29:29 -0500

    Connection Helper

---
+++ ContactPro/Helpers/ConnectionHelper.cs
@@ -0,0 +1,34 @@
+﻿using Npgsql;
+
+namespace ContactPro.Helpers
+{
+    public static class ConnectionHelper
+    {
+        public static string GetConnectionString(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetSection("pgSettings")["pgConnection"];
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+            return string.IsNullOrEmpty(databaseUrl) ? connectionString : databaseUrl;
+        }
+
+        //build a connection string from the environment. i.e. Railway
+        private static string BuildConnectionString(string databaseUrl)
+        {
+            var databseUri = new Uri(databaseUrl);
+            var userInfo = databseUri.UserInfo.Split(':');
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = databseUri.Host,
+                Port = databseUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databseUri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Require,
+                TrustServerCertificate = true
+            };
+
+            return builder.ToString();
+        }
+    }
+}
